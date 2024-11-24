d03550c8c76d752445a6bd1e8eacd56211dd2e9e
msk-chaithanya-raj <123654343+msk-chaithanya-raj@users.noreply.github.com>
2024-11-24 11:22:34 +0530

    Updated Feeback error

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -40,7 +40,7 @@ const Feedback = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/feedback", {
+      const response = await fetch("https://healu-backend.onrender.com/api/feedback", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
